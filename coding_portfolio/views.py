fa62956ef8fd3ac98075da5f419f8b098243b45f
Alex Petrenko <alex.petrenko612@outlook.com>
2016-11-21 10:05:19 +0200

    My Website App, first commit

---
+++ coding_portfolio/views.py
@@ -0,0 +1,102 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from coding_portfolio.models import Project, Language, About
+
+
+# TODO: Be able to show <code> or <pre> blocks as code in posts. - Need help with this!
+
+
+################################################################################
+#                                                                              #
+#    Copyright (C) 2016  Areeb Beigh <areebbeigh@gmail.com>                    #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                              #
+################################################################################
+
+
+def index(request):
+    """ Index view, returns a list of recent projects in the context """
+    recent_projects = []
+    for i in range(0, 6):
+        try:
+            recent_projects.append(Project.objects.order_by('-publish_date')[i])
+        except IndexError:
+            break
+    context = {
+        'projects': recent_projects,
+    }
+    return render(request, 'index.html', context)
+
+
+def project_list(request, language):
+    """ Returns the projects list for a given language """
+    language_object = get_object_or_404(Language, name=language)
+    paginator = Paginator(language_object.project_set.order_by('-publish_date'), 6)
+
+    if 'page' not in request.GET:
+        page = 1
+    else:
+        page = request.GET['page']
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        projects = paginator.page(1)
+    except EmptyPage:
+        projects = paginator.page(paginator.num_pages)
+
+    context = {
+        'projects': projects,
+        'language': language,
+    }
+    return render(request, 'projects_page.html', context)
+
+
+def project_detail(request, project_id):
+    """ Returns view for individual project pages """
+    project_object = get_object_or_404(Project, id=project_id)
+    context = {
+        'project': project_object,
+    }
+    return render(request, 'project_detail.html', context)
+
+def search(request):
+    """ Search view """
+    if "q" in request.GET:
+        queries = request.GET['q'].split()
+        projects = []
+        for q in queries:
+            projects.extend(list(Project.objects.filter(name__icontains=q)))
+            try:
+                # If a number is given, include the project with that ID if any
+                q = int(q)
+                projects.extend(Project.objects.get(id=q))
+            except ValueError:
+                pass
+
+        context = {
+            'projects': projects,
+        }
+        return render(request, 'search_page.html', context)
+    else:
+        return HttpResponseRedirect("/")
+
+def about(request):
+    """ About view """
+    try:
+        about = About.objects.get().description
+    except:
+        about = "No information here yet."
+
+    return render(request, 'about_page.html', {'about': about})
